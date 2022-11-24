2e51421b8f5032a567e81efd6701bcdcc473dfa8
miraz-ezaz <miraz.ezaz@gmail.com>
2022-11-24 22:52:03 +0600

    c

--- LabAssignment/src/main/java/com/mycompany/labassignment/Customer.java
+++ LabAssignment/src/main/java/com/mycompany/labassignment/Customer.java
@@ -4,6 +4,8 @@
  */
 package com.mycompany.labassignment;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author User
@@ -14,18 +16,24 @@ public class Customer {
     String Address;
     int PhNo;
     Cart cart;
+    ArrayList<Products> productList;
+    Payment payment;
 
     public Customer(String Id, String Name, String Address, int PhNo) {
         this.Id = Id;
         this.Name = Name;
         this.Address = Address;
         this.PhNo = PhNo;
-        this.cart = new Cart(Id)
+        this.cart = new Cart(Integer.parseInt(Id));
+        this.productList = new ArrayList<Products>();
+        this.payment = new Payment();
     }
 
    
     
     public void BuyProducts(Products product){
+        
+        System.out.println("Product :" + product.Name + "Buying Successfull");
         
     }
     
@@ -33,7 +41,12 @@ public class Customer {
         
     }
     
-    public void MakePayment(){
+    public void MakePayment(String CardType, String CardNo){
+        this.payment.setCardNo(CardNo);
+        this.payment.setCardType(CardType);
+        this.payment.setName(this.Name);
+        this.payment.setCustomerId(this.Id);
+        this.payment.setPrice(this.cart.Total);
         
     }
     
--- LabAssignment/src/main/java/com/mycompany/labassignment/Payment.java
+++ LabAssignment/src/main/java/com/mycompany/labassignment/Payment.java
@@ -13,17 +13,45 @@ public class Payment {
     String Name;
     String CardType;
     String CardNo;
-
+    float price;
     public Payment(String CustomerId, String Name, String CardType, String CardNo) {
         this.CustomerId = CustomerId;
         this.Name = Name;
         this.CardType = CardType;
         this.CardNo = CardNo;
     }
+
+    public Payment() {
+    }
     
     public void MakePayment(){
         
+        System.out.println("Payment Successfull");
+        System.out.println("Total: "+this.price );
+        
     }
+
+    public void setCustomerId(String CustomerId) {
+        this.CustomerId = CustomerId;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public void setCardType(String CardType) {
+        this.CardType = CardType;
+    }
+
+    public void setCardNo(String CardNo) {
+        this.CardNo = CardNo;
+    }
+    
+    
 
    
 }
