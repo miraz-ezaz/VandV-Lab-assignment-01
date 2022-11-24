4d44e3134a241804950d3fcb90c26b36e71f3671
miraz-ezaz <miraz.ezaz@gmail.com>
2022-11-25 00:14:13 +0600

    a

--- LabAssignment/src/main/java/com/mycompany/labassignment/LabAssignment.java
+++ LabAssignment/src/main/java/com/mycompany/labassignment/LabAssignment.java
@@ -9,6 +9,31 @@ package com.mycompany.labassignment;
 public class LabAssignment {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to Online Shoping System");
+        
+        Admin admin = new Admin(1, "Admin");
+        System.out.println("Admin Page");
+        System.out.println("Admin Name: "+admin.Name);
+        Products product1 = new Products(0, "Carrot", "Vegetable", "Winter Veg", 100);
+        Products product2 = new Products(1, "Cabbage", "Vegetable", "Winter Veg", 80);
+        
+        
+        admin.AddProducts(product1);
+        admin.AddProducts(product2);
+        
+        System.out.println("Product Added");
+        
+        System.out.println("ProductList Page");
+        admin.ViewProducts();
+        
+        
+        
+        System.out.println("Guest Page");
+        Guest guest = new Guest();
+        
+        guest.GetRegistered("1", "Miraj", "Sylhet", 123456);
+        
+        
+        
     }
 }
---
+++ LabAssignment/src/main/java/com/mycompany/labassignment/ProductList.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.labassignment;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author User
+ */
+public class ProductList {
+    ArrayList<Products> productList;
+    Products product1 = new Products(0, "Carrot", "Vegetable", "Winter Veg", 100);
+    Products product2 = new Products(1, "Cabbage", "Vegetable", "Winter Veg", 80);
+
+    public ProductList() {
+        
+        productList = new ArrayList<Products>();
+        productList.add(product1);
+        productList.add(product2);
+                
+        
+    }
+    
+    
+    
+}
